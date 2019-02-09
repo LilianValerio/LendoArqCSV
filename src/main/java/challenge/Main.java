7863e5e09bde849078f899b47d2c7e271576aa10
Lilian Santos <34148536+LilianValerio@users.noreply.github.com>
2019-02-09 07:51:38 -0200

    "commit"

--- src/main/java/challenge/Main.java
+++ src/main/java/challenge/Main.java
@@ -1,39 +1,38 @@
 package challenge;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import challenge.beans.Players;
 
 public class Main {
-	
-	
+
+
+    List<Players> players = readListCsv();
 
 	// Quantas nacionalidades (coluna `nationality`) diferentes existem no arquivo?
 	public int q1() {
-		List<Players> players = readListCsv();
-		
+        players = readListCsv();
 		return ((int) players
 				.stream()
 				.map(Players::getNationality)
 				.distinct()
 				.count());
-		
+
 	}
 
 	// Quantos clubes (coluna `club`) diferentes existem no arquivo?
 	// Obs: Existem jogadores sem clube.
 	public int q2() {
-		List<Players> players = readListCsv();
+        players = readListCsv();
 		return ((int) players
 				.stream()
 				.filter(club -> !club.getClub().isEmpty())
@@ -44,89 +43,98 @@ public class Main {
 
 	// Liste o primeiro nome (coluna `full_name`) dos 20 primeiros jogadores.
 	public List<String> q3() {
-		List<Players> players = readListCsv();
-
+        players = readListCsv();
 		return (players
 				.stream()
 				.filter(name -> !name.getFull_name().isEmpty()).limit(20)
-				.map(Players::getFull_name).collect(Collectors.toList()));	
-		}
-		
+				.map(Players::getFull_name).collect(Collectors.toList()));
+	}
+
 	// Quem são os top 10 jogadores que possuem as maiores cláusulas de rescisão?
 	// (utilize as colunas `full_name` e `eur_release_clause`)
 	public List<String> q4() {
-		List<Players> players = readListCsv();
+        players = readListCsv();
 
-		//falta transforma o getur em integer, senao a logica nao compara
-		
 		return (players.stream()
 				.filter(name -> !name.getFull_name().isEmpty())
-				.sorted(Comparator.comparing(Players::getEur_release_clause).reversed())
+				.sorted(Comparator.comparing(Players::getEur_release_clause,Comparator.nullsLast(Comparator.reverseOrder())))
 				.limit(10)
-				.map(Players::getFull_name).collect(Collectors.toList()));	
+				.map(Players::getFull_name).collect(Collectors.toList()));
 	}
 
 	// Quem são os 10 jogadores mais velhos (use como critério de desempate o campo `eur_wage`)?
 	// (utilize as colunas `full_name` e `birth_date`)
 	public List<String> q5() {
-		return null;
+        players = readListCsv();
+
+        return (players.stream()
+                .filter(name -> !name.getFull_name().isEmpty())
+                .sorted(Comparator.comparing(Players::getBirth_date).thenComparingDouble(Players::getEur_wage))
+                .limit(10)
+                .map(Players::getFull_name).collect(Collectors.toList()));
 	}
 
 	// Conte quantos jogadores existem por idade. Para isso, construa um mapa onde as chaves são as idades e os valores a contagem.
 	// (utilize a coluna `age`)
 	public Map<Integer, Integer> q6() {
-		return null;
+        players = readListCsv();
+
+	    List<Integer> idades = players.stream().mapToInt(p->p.getAge()).distinct().boxed().collect(Collectors.toList());
+        Map<Integer,Integer> mapaIdade = idades.stream().collect(
+                Collectors.toMap(Function.identity(), e -> (int)players.stream().filter(p->p.getAge()==e).count()));
+		return mapaIdade;
 	}
-	
+
 	public List<Players> readListCsv() {
-		 
-         List<Players> players = new ArrayList<>();
-				                                    
-         try {
-        	String linesArchive = new String(); 
-        	File archiveCsv = new File("src/main/resources/data.csv");
-			Scanner reading = new Scanner(archiveCsv);
-			
-			int id = 0;
-			reading.nextLine();
-			while(reading.hasNext()) {
-				
-				linesArchive = reading.nextLine();
-				String[] values = linesArchive.split(",");
-				Players player =
-				new Players(values[0], values[1], values[2], values[14], values[3], new BigDecimal(values[18])
-						, (new Double(values[17])), (LocalDate.parse(values[8])), (Integer.parseInt(values[6])));
-				
-				players.add(player);
-				id = id+1;
-				System.out.println(id);
-			}
-	
-		} catch (FileNotFoundException e) {
-		
+
+	    if(players==null){
+            players = new ArrayList<>();
+        }
+	    else{
+	        return players;
+        }
+		try {
+
+            InputStream resourceStream  =
+                    Players.class.getResourceAsStream("/data.csv");
+
+            BufferedReader reading = new BufferedReader(new InputStreamReader(resourceStream));
+            String[] lines = reading.lines().toArray(String[]::new);
+            lines = Arrays.copyOfRange(lines, 1, lines.length);
+			Locale in_ID = new Locale("en","US");
+			DecimalFormat nf = (DecimalFormat) NumberFormat.getInstance(in_ID);
+			nf.setParseBigDecimal(true);
+			for(String line : lines){
+                String[] values = line.split(",");
+
+                BigDecimal bd = BigDecimal.ZERO;
+                Double doubleValue = new Double(0);
+                Integer integerValue = new Integer(0);
+                LocalDate localDateValue = LocalDate.now();
+                try {
+                    bd = (BigDecimal)nf.parse(values[18], new ParsePosition(0));
+                    doubleValue = Double.valueOf(values[17]);
+                    localDateValue = LocalDate.parse(values[8]);
+                    integerValue = Integer.parseInt(values[6]);
+
+                }catch (Exception e){
+                }
+                Players player =
+                        new Players(values[0], values[1], values[2], values[14], values[3],
+                                bd
+                                , doubleValue, localDateValue, integerValue);
+
+                players.add(player);
+
+            }
+
+
+		} catch (Exception e) {
+            e.printStackTrace();
 		}
-         
-	      return players;
-	       
+
+		return players;
+
 	}
-	
-	public static void main(String[] args){
-		
-		Main main = new Main();
-		int teste1 = main.q1();
-		int teste2 = main.q2();
-		List<String> teste3 = main.q3();
-		List<String> teste4 = main.q4();
-		List<String> teste5 = main.q5();
-		Map<Integer, Integer> teste6 = main.q6();
-		
-		
-		
-	 System.out.println("teste 1 - " + teste1);
-		 System.out.println("teste 2 - " + teste2);
-	 System.out.println("teste 3 - " + teste3);
-		 System.out.println("teste 4 - " + teste4);
-		 System.out.println("teste 5 - " + teste5);
-		 System.out.println("teste 6 - " + teste6);
-	}
+
 }
